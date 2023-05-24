300342921fbf8851d4a897f4191f82cef1230ad4
André Pereira <adbp@live.com.pt>
2023-05-24 17:21:27 +0100

    feat: update baseimage to add routes

--- baseimage/sleep.sh
+++ baseimage/sleep.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 if [[ -z "${DEFAULT_ROUTE}" ]]; then
-    echo "No default route"
+    echo "No DEFAULT_ROUTE"
 else
     /sbin/ip route replace default via $DEFAULT_ROUTE
 fi
+
+if [[ -z "${ROUTES}" ]]; then
+    echo "No ROUTES"
+else
+    arrROUTES=(${ROUTES//;/ })
+    for route in "${arrROUTES[@]}"; do
+        arrRoute=(${route//:/ })
+        /sbin/ip route a ${arrRoute[0]} via ${arrRoute[1]}
+    done
+fi
+
 # /bin/bash -c 'iptables -t filter -A FORWARD -p tcp --dport 80 ! -s 10.0.1.253 -j DROP'
 while true ; do /bin/sleep 5m; done
